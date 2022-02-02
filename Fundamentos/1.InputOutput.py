cf4561093aec3979d02e1b768cd89a905371aaa5
Anderson <anderson.cardenas.r@gmail.com>
2022-02-01 19:41:26 -0500

    Formulario

---
+++ Fundamentos/1.InputOutput.py
@@ -0,0 +1,20 @@
+# Función entrada y función de salida
+# Va a buscar las palabras reservas
+
+#Argumento a la función, '' -> string
+print('Formulario')
+print('Nombre: ', end='')
+# Input -> entrada datos
+nombre = input()
+print('Apellido: ', end='')
+apellido = input()
+print('Edad: ', end='')
+edad = input()
+print('El nombre ingresado es: ',nombre)
+print('El apellido ingresado es: ',apellido)
+print('Tu edad ingresada es: ',edad)
+
+
+# Python es un lenguaje 100% orientado a objetos
+
+
