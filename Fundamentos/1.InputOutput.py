ef2e89787ab404e88346bba12995e3ec265505f8
Anderson <anderson.cardenas.r@gmail.com>
2022-02-03 18:05:53 -0500

    Variables

--- Fundamentos/1.InputOutput.py
+++ Fundamentos/1.InputOutput.py
@@ -10,9 +10,14 @@ print('Apellido: ', end='')
 apellido = input()
 print('Edad: ', end='')
 edad = input()
+
+# Primer reto curso, implicita print
+ciudad = input('Ingresa tu ciudad: ')
+
 print('El nombre ingresado es: ',nombre)
 print('El apellido ingresado es: ',apellido)
 print('Tu edad ingresada es: ',edad)
+print('Tu ciudad ingresada es: ',ciudad)
 
 
 # Python es un lenguaje 100% orientado a objetos
---
+++ Fundamentos/2.Variables.py
@@ -0,0 +1,19 @@
+#Variables
+# No deben caracteres especiales, no deben empezar con mayusculas, no debe palabra reservada
+
+# Tipos de Variables en python
+# Python es un lenguaje de tipado débil
+# Inicialización de las variables
+# 4. El tipo de dato de las variables viene dado por su inicialización
+# cast -> casting de variables
+
+edad = 22 
+nombre = 'Anderson'
+# La declaración de os flotantes es con un punto
+ira = 37.2 
+mayorEdad = True
+
+print('El tipo de dato de la variable edad es: ',type(edad))
+print('El tipo de dato de la variable nombre es: ',type(nombre))
+print('El tipo de dato de la variable IRA es: ',type(ira))
+print('El tipo de dato de la variable mayor de edad es: ',type(mayorEdad))
