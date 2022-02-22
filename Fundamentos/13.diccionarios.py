dea803c6348db5ccb6391ea7cb42897375f7c5bf
Anderson <anderson.cardenas.r@gmail.com>
2022-02-22 18:21:14 -0500

    Diccionarios 1

---
+++ Fundamentos/13.diccionarios.py
@@ -0,0 +1,19 @@
+# Diccionarios  -> dict()
+# Hash maps
+# Tiene dos partes: clave(llave) y el valor
+# Tras la inicialización la clave es inmutable
+# Clave puede ser de cualquier tipo de dato
+# No soporta la indexación
+
+diccionario1 = {
+    'i1': 1,
+    'i2': 5
+}
+
+print(type(diccionario1))
+print(diccionario1)
+
+# Acceder a los datos de un diccionario
+print(diccionario1['i2'])
+
+
