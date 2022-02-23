3908fa2b4899e7676610968302fab9bc41061a94
Anderson <anderson.cardenas.r@gmail.com>
2022-02-22 19:17:15 -0500

    Diccionarios

--- Fundamentos/13.diccionarios.py
+++ Fundamentos/13.diccionarios.py
@@ -4,9 +4,7 @@
 # Tras la inicialización la clave es inmutable
 # Clave puede ser de cualquier tipo de dato al igual que el valor
 # No soporta la indexación
-
-from sklearn.metrics import precision_score
-
+# CRUD -< Create, update, read, delete
 
 diccionario1 = {
     'i1': 1,
@@ -50,7 +48,44 @@ print('Métodos')
 print(persona.get('edad'))
 # actualizar un dato
 print(persona.update({'edad':23}))
+# agregar valor a un diccionario ya creado
+persona['universidad'] = 'EPN'
 print(persona)
+#eliminar valores de un diccionario
+# pop eliminar a través de la llave
+persona.pop(21)
+print(persona)
+# eliminación del valor 
+
+# 1. Saber cuales son solo los valores del diccionario
+print(persona.values())
+# 2. Saber cuales son solo las llaver del diccionario
+print(persona.keys())
+# 3. Diccionario mapeado
+print(persona.items())
+# 
+def obtenerLlave(valorBuscar):
+    for llave, valor in persona.items():
+        if valorBuscar == valor:
+            return llave
+        #return Exception
+
+print('Llave encontrada:',obtenerLlave('1726744814'))
+persona.pop(obtenerLlave('1726744814'))
+print(persona)
+
+# pop item -> eliminar el último valor insertado en el diccionario (3.7)
+persona.popitem()
+print(persona)
+
+# ¿Qué pasatía si pido obtener un valor ?
+print(persona.get('mascota', 'No existe este valor'))
+
+copiaPersona = persona.copy()
+print(copiaPersona)
+
+# Numerable
+print(len(copiaPersona))
 
 # 
 #for i in listaPrueba:
