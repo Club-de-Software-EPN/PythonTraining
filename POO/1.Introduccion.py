ec1b90407f442e573a74f72e25d6fdc47b6b3253
Anderson <anderson.cardenas.r@gmail.com>
2022-03-04 18:52:32 -0500

    Introducción a POO

---
+++ POO/1.Introduccion.py
@@ -0,0 +1,30 @@
+# POO
+# Prototipo, plantilla
+
+# Clase 
+# Atributos (caracterísctica y métodos)
+# ADT -> Abstract Data Type
+
+class Curso:
+    # Atributos
+    # Métodos
+    pass
+
+class Alumno:
+    pass
+
+# Instanciar una clase -> objeto
+curso1 = Curso()
+curso2 = Curso()
+print(type(curso1))
+
+cursos = [curso1, curso2]
+print(cursos)
+
+
+texto = 'anderson'
+print(texto.capitalize())
+
+# POO en python
+print('Comprobar si curso1 es instancia de Curso: ', isinstance(curso1,Curso))
+print('Comprobar si curso1 es instancia de Alumno: ', isinstance(curso1,Alumno))
