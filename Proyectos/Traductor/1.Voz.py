c6c348687d11134fc018892e3eaf37f93acb06fc
Anderson <anderson.cardenas.r@gmail.com>
2022-03-08 20:00:35 -0500

    trductor

--- Proyectos/Traductor/1.Voz.py
+++ Proyectos/Traductor/1.Voz.py
@@ -1,7 +1,10 @@
 import pyttsx3
 
 motorVoz = pyttsx3.init()
+voices = motorVoz.getProperty('voices')
+print(voices)
 motorVoz.setProperty('rate',120)
+motorVoz.setProperty('voice',voices[0].id)
 texto = 'Hoy es el día de la mujer'
 motorVoz.say(texto)
 motorVoz.runAndWait()
--- Proyectos/Traductor/2.Traductor.py
+++ Proyectos/Traductor/2.Traductor.py
@@ -1,2 +1,21 @@
+from googletrans import Translator
+import pyttsx3
 
+motorVoz = pyttsx3.init()
+traductor = Translator()
+motorVoz.setProperty('rate',120)
 
+try:
+    with open('Proyectos/Traductor/libro.txt',mode='r') as miArchivo:
+        # Realziar traducción
+        texto = miArchivo.read()
+        textoTraducido = traductor.translate(text=texto).text
+        # Escribir la traducción en un txt
+        txtsalida = open('Proyectos/Traductor/libroTraducido.txt','w')
+        txtsalida.write(textoTraducido)
+        # Lee el audiolibro
+        motorVoz.say('Audiolibro traducido de 100 años de soledad')
+        motorVoz.say(textoTraducido)
+        motorVoz.runAndWait()
+except FileNotFoundError as error:
+    print('Error: ', error)
