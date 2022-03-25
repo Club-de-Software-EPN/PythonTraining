22dd2ab58c5f00e84e63f9d9d20e91a3a92039db
Anderson <anderson.cardenas.r@gmail.com>
2022-03-25 18:31:51 -0500

    Center win e interfaz

--- Tkinter/3.Tkinter.py
+++ Tkinter/3.Tkinter.py
@@ -1,27 +1,24 @@
 import tkinter as tk
 from tkinter import GROOVE, SUNKEN, ttk
-
-def center(win): 
-    """ centers a tkinter window :param win: the main window or Toplevel window to center """ 
-    win.update_idletasks() 
-    width = win.winfo_width() 
-    frm_width = win.winfo_rootx() - win.winfo_x() 
-    win_width = width + 2 * frm_width 
-    height = win.winfo_height() 
-    titlebar_height = win.winfo_rooty() - win.winfo_y() 
-    win_height = height + titlebar_height + frm_width 
-    x = win.winfo_screenwidth() // 2 - win_width // 2 
-    y = win.winfo_screenheight() // 2 - win_height // 2 
-    win.geometry('{}x{}+{}+{}'.format(width, height, x, y)) 
-    win.deiconify()
+from utilidades import center
 
 colorGris = '#B8B9AA'
 colorBlanco = '#F7F7F7'
 colorVerde = '#1BE518'
 colorAmarillo = '#F7FF00'
+colorAmarilloOscuro = '#E6DD08'
 
-def main():
-    root = tk.Tk()
-    root.title('Ejercicio 1')
-    root.geometry('800x550')
-    root.resizable(width=True, height=True)
+class Ventana:
+    def __init__(self, titulo) -> None:
+        self.titulo = titulo
+        self.dibujar()
+    def dibujar(self):
+        root = tk.Tk()
+        root.title(self.titulo)
+        root.geometry('800x550')
+        root.resizable(width=True, height=True)
+        center(root)
+        root.mainloop()
+
+ventana = Ventana('Ejercicio 1')
+
